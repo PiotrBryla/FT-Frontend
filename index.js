f0c2daeb9aaa3641991b968b34e1da28b3836361
PiotrBryla <piotrbryla10@gmail.com>
2018-03-03 13:30:11 +0000

    .gitignore, responsive header, first API call parse

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Import necessary modules
 const express = require('express');
 const path = require('path');
+const request = require('request');
 
 // Initialize application
 const app = express();
+
+// FT API Key
+const API_KEY = "";
 
 // Set view engine
 app.set ('view engine', 'pug') ;
@@ -18,6 +22,27 @@ app.set('views', [
 
 // Home API Call
 app.get('/' ,function(req, res){
+
+    let query = {
+        "queryString": "Weather",
+        "resultContext" : {
+		 "aspects" :[  "title","lifecycle","location","summary","editorial" ]
+	    }
+    };
+
+    request({
+        url: "http://api.ft.com/content/search/v1",
+        method: "POST",
+        json: true,
+        headers: { "X-Api-Key": API_KEY, "Content-Type": "application/json"},
+        body: query
+    }, function (error, response, reqBody){
+        // Get title of article
+        // response.body.results[0].results[0].title.title
+        console.log(response.body.results[0].results[0]);
+    });
+
+
     res.render('index');
 });
 
